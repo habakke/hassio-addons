003ce9fb55f59f1808ad46747303507ed69fb53f
Håvard Bakke <habakke@matrise.net>
2020-06-28 19:06:53 +0200

    Changed to home-assistant alpine to debian base image

--- deconz/build.sh
+++ deconz/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-docker build -t armhf-addon-deconz:5.3.6 --build-arg BUILD_ARCH=armhf --build-arg BUILD_FROM=homeassistant/armhf-base --build-arg DECONZ_VERSION=2.05.78 . \
-&& docker tag armhf-addon-deconz:5.3.6 habakke/armhf-addon-deconz:5.3.6 \
-&& docker push habakke/armhf-addon-deconz:5.3.6
+VERSION=5.3.6
+DECONZ_VERSION=2.05.78
+docker build -t armhf-addon-deconz:${VERSION} --build-arg BUILD_ARCH=armhf --build-arg BUILD_FROM=homeassistant/armv7-base-debian:latest --build-arg DECONZ_VERSION=${DECONZ_VERSION} . \
+&& docker tag armhf-addon-deconz:${VERSION} habakke/armhf-addon-deconz:${VERSION} \
+&& docker tag armhf-addon-deconz:latest habakke/armhf-addon-deconz:latest \
+&& docker push habakke/armhf-addon-deconz:${VERSION}
